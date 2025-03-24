e8cee5895bac63ff86fe2f2eb3a259f8ed0b8fdb
hammer-dev-sh <65677890+hammer-dev-sh@users.noreply.github.com>
2025-03-24 19:05:50 -0400

    initial commit

---
+++ R365CalculatorChallenge/R365CalculatorChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace R365CalculatorChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
