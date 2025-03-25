cd06aa72c00d74ef9a49afc9503a8c553646fc57
hammer-dev-sh <65677890+hammer-dev-sh@users.noreply.github.com>
2025-03-25 19:02:29 -0400

    added DI

--- R365CalculatorChallenge/R365CalculatorChallenge/Program.cs
+++ R365CalculatorChallenge/R365CalculatorChallenge/Program.cs
@@ -1,4 +1,6 @@
-﻿using R365CalculatorChallenge.Exceptions;
+﻿using Microsoft.Extensions.DependencyInjection;
+using R365CalculatorChallenge.Exceptions;
+using R365CalculatorChallenge.Interfaces;
 using R365CalculatorChallenge.Services;
 using System.Text.RegularExpressions;
 
@@ -12,29 +14,15 @@ namespace R365CalculatorChallenge
 
             try
             {
-                // in this stretch goal, we want to allow the user to send in arguments to:
-                //  add an alternative delimiter (step #3)
-                //  toggle whether to deny negative numbers (step #4)
-                //  change/remove upper bound limit (step #5)
+                // stretch goal for implementing DI
+                // this makes it easy to swap out for future implementations of subtractionService, multiplyService, etc.
+                var serviceProvider = new ServiceCollection()
+                    .AddSingleton<ICalculationService, AdditionService>()
+                    .BuildServiceProvider();
 
-                
+                var calculationService = serviceProvider.GetService<ICalculationService>();
 
-                // call the calculation function
-                // we want to make sure we unescape the userinput so eliminate the escape characters added automatically
-                // we will also use named parameters to show we can supply any optional parameter we want, having
-                //  the rest be their default values (per the original spec)
-
-                var resultUpperDefault = additionService.Calculate(Regex.Unescape("2,1001,6"));   // upper bound=1000 (default)
-                var resultUpperIncreased = additionService.Calculate(Regex.Unescape("2,1001,6"), upperBoundLimit: 4000);   // upper bound=4000
-
-                // commenting below out because it throws an exception
-                //var resultDenyNegative = additionService.Calculate(Regex.Unescape("4,-3"));
-                var resultAllowNegative = additionService.Calculate(Regex.Unescape("4,-3"), denyNegativeNumbers: false);
-
-                // display full formula
-                Console.WriteLine($"Result (UpperLimitRestraintDefault): {resultUpperDefault.FormulaWithAnswer}");
-                Console.WriteLine($"Result (UpperLimitRestraintIncreased): {resultUpperIncreased.FormulaWithAnswer}");
-                Console.WriteLine($"Result (AllowNegative): {resultAllowNegative.FormulaWithAnswer}");
+                Console.WriteLine(calculationService.Calculate("//[*][!!][r9r]\n11r9r22*hh*33!!44").FormulaWithAnswer);
             }
             catch (InvalidInputException ex)
             {
